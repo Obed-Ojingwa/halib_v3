42f37721c7f8bca3611e163c32e32758cf654115
Obed-Ojingwa <obedojingwa@gmail.com>
2026-06-19 12:33:07 +0100

    Modified the Admin Orders for Vercel build

--- frontend/src/components/admin/AdminOrders.tsx
+++ frontend/src/components/admin/AdminOrders.tsx
@@ -244,7 +244,7 @@ export default function AdminOrders() {
                       </td>
                     </tr>
                   )}
-                </>
+                </Fragment>
               ))}
             </tbody>
           </table>
